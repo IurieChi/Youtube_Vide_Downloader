60feb1510795ef30efda89c1617404072d34b537
IurieChi <chigaiiura@yahoo.com>
2024-02-05 20:28:13 +0100

    added popup check  if link orr folder are empty

--- gui.py
+++ gui.py
@@ -18,17 +18,20 @@ while True:
     event , value = window.read()
     new_path = value['path']
     link = value['link']
-    if event == 'Save Video':
-        window['-OUTPUT-'].update(l.download_video(link,new_path),text_color='yellow')
-    
-    if event == 'Save Music':
-        window['-OUTPUT-'].update(l.download_audio(link,new_path),text_color='white')
 
-    if event == 'Cancel':
-        window['-OUTPUT-'].update("")
-    
-
-        
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
+
+    if value['link'] != '':
+        if value['path']  !='':
+            if event == 'Save Video':
+                window['-OUTPUT-'].update(l.download_video(link,new_path),text_color='yellow')
+            
+            if event == 'Save Music':
+                window['-OUTPUT-'].update(l.download_audio(link,new_path),text_color='white')
+        else:
+            sg.popup('      Warning!     ','Please select folder',text_color='yellow',button_color = 'red')
+    else:
+        sg.popup('Warning!','Please add link',text_color='yellow',button_color = 'red')
+    
 window.close()
